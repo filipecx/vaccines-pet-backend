d8c53991b5c5b780e0592ed3ef436f577ff8740c
Filipe C <filipecavalcantiximenes@gmail.com>
2025-09-25 22:35:21 -0300

    :card_file_box: pet repository

--- src/Repositories/dbPetRepository.ts
+++ src/Repositories/dbPetRepository.ts
@@ -1,26 +1,61 @@
 import { Pet } from "../Core/Domain/Pet/Entity/Pet";
 import { PetRepository } from "./petRepository";
+import { PrismaClient } from "@prisma/client";
+
 
 export class DbPetRepository implements PetRepository {
+    constructor(private prisma: PrismaClient) {}
+
+    public static create(prismaClient: PrismaClient) {
+        return new DbPetRepository(prismaClient);
+    }
     
     async createPet(pet: Pet): Promise<void> {
-        
+        await this.prisma.pet.create({
+            data: {
+                name: pet.name,
+                image: pet.image,
+                active: pet.active
+            }
+        })
     }
 
     async getPetById(id: number): Promise<Pet> {
-        
+        const data: any = await this.prisma.pet.findUnique({where: {id: id}})
+        const pet: Pet = new Pet({
+            name: data.name,
+            image: data.image,
+            active: data.active
+        })
+        return pet
     }
 
-    async getPetByVaccine(vaccineId: number): Promise<Pet> {
-        
+    async getAllPets(): Promise<Pet[]> {
+        const dataList = await this.prisma.pet.findMany()
+        const petsList = dataList.map((pet) => {
+            return new Pet({
+                name: pet.name,
+                image: pet.image,
+                active: pet.active
+            })
+        })
+        return petsList;
+
     }
 
-    async updatePet(pet: Pet): Promise<void> {
-        
+    async updatePet(pet: Pet, id: number): Promise<void> {
+        await this.prisma.pet.update({
+            where: {id: id},
+            data: {
+                name: pet.name,
+                image: pet.image,
+                active: pet.active
+            }
+        })
     }
 
-    async changePetActiveById(id: number): Promise<void> {
-        
+    async changePetActiveById(id: number, active: boolean): Promise<void> {
+        await this.prisma.pet.update({where: {id: id}, data: {active: active}})
     }
 
 
