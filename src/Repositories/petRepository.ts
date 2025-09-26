afbfc294d3bbb95831cd56993c99b6a283508066
Filipe C <filipecavalcantiximenes@gmail.com>
2025-09-25 22:37:02 -0300

    :card_file_box: pet repository

--- src/Repositories/petRepository.ts
+++ src/Repositories/petRepository.ts
@@ -2,9 +2,9 @@ import { Pet } from "../Core/Domain/Pet/Entity/Pet";
 
 export interface PetRepository {
     createPet(pet: Pet): Promise<void>
-    getPetById(id: number): Promise<Pet>
-    getPetByVaccine(vaccineId: number): Promise<Pet>
-    updatePet(pet: Pet): Promise<void>
-    changePetActiveById(id: number): Promise<void>
+    getPetById(id: number): Promise<Pet | null>
+    getAllPets(): Promise<Pet[]>
+    updatePet(pet: Pet, id: number): Promise<void>
+    changePetActiveById(id: number, active: boolean): Promise<void>
     
 }
