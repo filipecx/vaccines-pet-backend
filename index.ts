36a33c702ccabd532299305fe7713ecfa5a469d7
Filipe C <filipecavalcantiximenes@gmail.com>
2025-09-25 22:38:02 -0300

    :sparkles: server and db connection

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log('ai')
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const app = express()
+const port = 3000
+const prisma = new PrismaClient
+
+async function start() {
+    await prisma.$connect();
+    console.log('prisma connected')
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}. You better catch it!`)
+    })
+}
+
+start()
+
+
+
