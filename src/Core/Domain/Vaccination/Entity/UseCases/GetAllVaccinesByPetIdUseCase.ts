1dd985b0accb7dd5ed00d933c3e125e536e12f53
Filipe C <filipecavalcantiximenes@gmail.com>
2025-09-27 11:55:34 -0300

    :pencil2: fixed typo on usecase name

--- src/Core/Domain/Vaccination/Entity/UseCases/GetAllVaccinesByPetIdUseCase.ts
+++ src/Core/Domain/Vaccination/Entity/UseCases/GetAllVaccinesByPetIdUseCase.ts
@@ -2,7 +2,7 @@ import { VaccinationRepository } from "../../Repository/vaccinationRepository";
 import { VaccinationErrors } from "../Errors/VaccinationErrors";
 import { Vaccinations } from "../Vaccinations";
 
-export class getAllVaccinesByPetId {
+export class GetAllVaccinesByPetIdUseCae {
     constructor(private vaccinationRepository: VaccinationRepository){}
 
     async execute(id: number): Promise<Vaccinations[]> {
